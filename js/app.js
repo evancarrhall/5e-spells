6a257191f7d4ba069925a67f4096295f9d72c46c
Evan Carr-Hall <evancarrhall@gmail.com>
2017-12-18 15:06:53 -0500

    add loading spinner

--- js/app.js
+++ js/app.js
@@ -22,7 +22,7 @@ var SpellCard = {
         componentsString() {
             return this.spell.components.length === 1 || this.spell.components === "V" || this.spell.components === "S" || this.spell.components === "M"
                 ? this.spell.components[0]
-                : this.spell.components.join(', ')
+                : this.spell.components.join(', ');
         },
 
         levelAndSchoolText() {
@@ -78,6 +78,7 @@ var vm = new Vue({
     data: {
         searchString: '',
         spellList: [],
+        loading: false
     },
     computed: {
         searchKeywords() {
@@ -114,13 +115,30 @@ var vm = new Vue({
         }
     },
     mounted() {
+        
+        let html =
+        `<div>
+            <svg version="1.1" id="loader-1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+            width="40px" height="40px" viewBox="0 0 40 40" enable-background="new 0 0 40 40" xml:space="preserve">
+                <path opacity="0.2" fill="#000" d="M20.201,5.169c-8.254,0-14.946,6.692-14.946,14.946c0,8.255,6.692,14.946,14.946,14.946
+                    s14.946-6.691,14.946-14.946C35.146,11.861,28.455,5.169,20.201,5.169z M20.201,31.749c-6.425,0-11.634-5.208-11.634-11.634
+                    c0-6.425,5.209-11.634,11.634-11.634c6.425,0,11.633,5.209,11.633,11.634C31.834,26.541,26.626,31.749,20.201,31.749z"/>
+                <path opacity="0.5" fill="#000" d="M26.013,10.047l1.654-2.866c-2.198-1.272-4.743-2.012-7.466-2.012h0v3.312h0
+                    C22.32,8.481,24.301,9.057,26.013,10.047z">
+                </path>
+            </svg>
+        </div>`;
+        let el = document.querySelector('.loadingspinner');
+        el.innerHTML = html;
+        this.loading = true;
         SPELLS.then(response => {
             return response.json();
         })
         .then(json => {
             json.forEach(spell => { 
                 this.spellList.push(spell);
-            })
+            });
+            this.loading = false;
         })
     }
 })
